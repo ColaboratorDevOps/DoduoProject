34783953cf8d4bb43d75d0ded85e4c11380780ff
Daniel Pirvu <daniel.catalin.pirvu@gmail.com>
2019-09-17 09:57:58 +0300

    Reset to v1.0.0-SNAPSHOT

--- GitResetToV1.0.0-SNAPSHOT.sh
+++ GitResetToV1.0.0-SNAPSHOT.sh
@@ -7,3 +7,5 @@ cat pom.initial.xml > pom.xml
 git add .
 git commit -m "Reset to v1.0.0-SNAPSHOT"
 git push https://ColaboratorDevOps:DevOpsTeam1@github.com/ColaboratorDevOps/DoduoProject.git
+
+git pull origin master
